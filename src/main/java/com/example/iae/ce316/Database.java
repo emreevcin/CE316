b719ea6253426c1c60c3d5209750655710dc6c15
emreevcin <emreevcin@icloud.com>
2023-05-11 18:49:32 +0300

    Database Class is updated.

--- src/main/java/com/example/iae/ce316/Database.java
+++ src/main/java/com/example/iae/ce316/Database.java
@@ -57,6 +57,56 @@ public class Database {
         }
     }
 
+    public ArrayList<Configuration> getAllConfigurations() throws SQLException {
+        ArrayList<Configuration> configurations = new ArrayList<>();
+        String query = "SELECT " +
+                "configuration_title, " +
+                "programming_language, " +
+                "lecturer_code_path, " +
+                "lib, " +
+                "args " +
+                "FROM configurations";
+
+        selectSQL = conn.prepareStatement(query);
+        ResultSet rs = selectSQL.executeQuery();
+
+        while (rs.next()) {
+            String title = rs.getString("configuration_title");
+            String language = rs.getString("programming_language");
+            String codePath = rs.getString("lecturer_code_path");
+            String lib = rs.getString("lib");
+            String args = rs.getString("args");
+            Configuration configuration = new Configuration(title, language, lib, args, codePath);
+            configurations.add(configuration);
+        }
+
+        rs.close();
+        selectSQL.close();
+
+        return configurations;
+    }
+
+    public ArrayList<Project> getAllProjects() throws SQLException {
+        ArrayList<Project> projects = new ArrayList<>();
+        String query = "SELECT project_title, configuration_id FROM projects";
+
+        selectSQL = conn.prepareStatement(query);
+        ResultSet rs = selectSQL.executeQuery();
+
+        while (rs.next()) {
+            String title = rs.getString("project_title");
+            int configurationID = rs.getInt("configuration_id");
+            Configuration c = getConfigurationByID(configurationID);
+            Project p = new Project(title, c);
+            projects.add(p);
+        }
+
+        rs.close();
+        selectSQL.close();
+
+        return projects;
+    }
+
     // It returns a project_title from projects table
     public ArrayList<String> getProjectTitles() throws SQLException {
         ArrayList<String> projectTitles = new ArrayList<>();
@@ -77,7 +127,7 @@ public class Database {
     }
 
     // It returns a configuration_title from configurations table
-    public ArrayList<String> getCfgTitles() throws SQLException {
+    public ArrayList<String> getConfigurationTitles() throws SQLException {
         ArrayList<String> cfgTitles = new ArrayList<>();
         String query = "SELECT configuration_title FROM configurations";
 
@@ -95,44 +145,78 @@ public class Database {
         return cfgTitles;
     }
 
-    // It returns a configuration_id from configurations table using configuration_title
-    private int getCfgID(Configuration configuration) throws SQLException {
-        String query =  "SELECT configuration_id " +
-                        "FROM configurations " +
-                        "WHERE configuration_title = ?";
+    private Configuration getConfigurationByID(int id) throws SQLException {
+        Configuration configuration = null;
+        String query = "SELECT " +
+                "configuration_id, " +
+                "configuration_title, " +
+                "programming_language, " +
+                "lecturer_code_path, " +
+                "lib, " +
+                "args " +
+                "FROM configurations " +
+                "WHERE configuration_id = ?";
 
-        selectSQL = conn.prepareStatement(query);
-        selectSQL.setString(1, configuration.getTitle());
+        selectSQL = conn.prepareCall(query);
+        selectSQL.setInt(1, id);
 
         ResultSet rs = selectSQL.executeQuery();
-        int configurationID = rs.getInt("configuration_id");
 
-        rs.close();
+        if (rs.next()) {
+            String title = rs.getString("configuration_title");
+            String language = rs.getString("programming_language");
+            String codePath = rs.getString("lecturer_code_path");
+            String lib = rs.getString("lib");
+            String args = rs.getString("args");
+
+            configuration = new Configuration(title, language, lib, args, codePath);
+
+        }
+
         selectSQL.close();
+        rs.close();
 
-        System.out.println(configurationID);
-
-        return configurationID;
+        return configuration;
     }
 
-    // It is an insert query to create an entity in projects table
-    public void addProject(Project project) throws SQLException {
-        int cfgID = getCfgID(project.getConfiguration());
+        // It returns a configuration_id from configurations table using configuration_title
+        private int getConfigurationID (Configuration configuration) throws SQLException {
+            String query = "SELECT configuration_id " +
+                    "FROM configurations " +
+                    "WHERE configuration_title = ?";
 
-        insertSQL = conn.prepareStatement("INSERT INTO projects(project_title, configuration_id) VALUES (?, ?)");
-        insertSQL.setString(1, project.getTitle());
-        insertSQL.setInt(2, cfgID);
-        insertSQL.executeUpdate();
+            selectSQL = conn.prepareStatement(query);
+            selectSQL.setString(1, configuration.getTitle());
 
-        insertSQL.close();
+            ResultSet rs = selectSQL.executeQuery();
+            int configurationID = rs.getInt("configuration_id");
 
-        System.out.println("Project entity added successfully!");
+            rs.close();
+            selectSQL.close();
 
-    }
+            System.out.println(configurationID);
 
-    // It is an insert query to create an entity in configuration table
-    public void addCfg(Configuration c) throws SQLException {
-        insertSQL = conn.prepareStatement("INSERT INTO configurations(" +
+            return configurationID;
+        }
+
+        // It is an insert query to create an entity in projects table
+        public void addProject (Project project) throws SQLException {
+            int cfgID = getConfigurationID(project.getConfiguration());
+
+            insertSQL = conn.prepareStatement("INSERT INTO projects(project_title, configuration_id) VALUES (?, ?)");
+            insertSQL.setString(1, project.getTitle());
+            insertSQL.setInt(2, cfgID);
+            insertSQL.executeUpdate();
+
+            insertSQL.close();
+
+            System.out.println("Project entity added successfully!");
+
+        }
+
+        // It is an insert query to create an entity in configuration table
+        public void addConfiguration (Configuration c) throws SQLException {
+            insertSQL = conn.prepareStatement("INSERT INTO configurations(" +
                     "configuration_title, " +
                     "programming_language, " +
                     "lecturer_code_path, " +
@@ -140,25 +224,25 @@ public class Database {
                     "args) " +
                     "VALUES (?, ?, ?, ?, ?)");
 
-        insertSQL.setString(1, c.getTitle());
-        insertSQL.setString(2, c.getLang());
-        insertSQL.setString(3, c.getDirectory());
-        insertSQL.setString(4, c.getLib());
-        insertSQL.setString(5, c.getArgs());
-        insertSQL.executeUpdate();
+            insertSQL.setString(1, c.getTitle());
+            insertSQL.setString(2, c.getLang());
+            insertSQL.setString(3, c.getDirectory());
+            insertSQL.setString(4, c.getLib());
+            insertSQL.setString(5, c.getArgs());
+            insertSQL.executeUpdate();
 
-        insertSQL.close();
+            insertSQL.close();
 
-        System.out.println("Configuration entity added successfully!");
+            System.out.println("Configuration entity added successfully!");
 
-    }
-
-    private static Database instance = null;
-
-    public static Database getInstance() {
-        if (instance == null) {
-            instance = new Database();
         }
-        return instance;
-    }
+
+        private static Database instance = null;
+
+        public static Database getInstance () {
+            if (instance == null) {
+                instance = new Database();
+            }
+            return instance;
+        }
 }
